568a3a7fc1ca28af2cc18ba430880afd791fa84f
Can Turko <turkoglu.can@gmail.com>
2022-08-11 20:39:07 +0300

    Kenarları bilinen üçgenin alanını hesaplama eklendi

---
+++ UcgeninAlani/src/UcgeninAlaniHesap.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class UcgeninAlaniHesap {
+    public static void main(String[] args) {
+        double a,b,c,d,e;
+        Scanner input = new Scanner(System.in);
+        System.out.print("a kenarının ölçüsünü yazıp enter'a basınız:");
+        a = input.nextDouble();
+        System.out.print("b kenarının ölçüsünü yazıp enter'a basınız:");
+        b = input.nextDouble();
+        System.out.print("c kenarının ölçüsünü yazıp enter'a basınız:");
+        c = input.nextDouble();
+        d=(a+b+c)/2;
+        e=d*(d-a)*(d-b)*(d-c);
+        System.out.println("üçgenin alanı:"+e);
+
+    }
+}
