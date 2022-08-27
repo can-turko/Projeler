d7811183658b0834b7281c7dfb193416afa649f6
Can Turko <turkoglu.can@gmail.com>
2022-08-27 19:10:49 +0300

    Asal sayılar eklendi

---
+++ BirdenYuzeKadarAsalSayi/src/AsalSayilar.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class AsalSayilar {
+    public static void main(String[] args) {
+        int a=0, b = 0, c = 0;
+        for (int i = 100; i > 1; i--) {
+            for (int j = i-1; j > 1; j--) {
+                a = i % j;
+                if(a==0){break;}
+
+            }
+            if(a!=0) {
+                System.out.println(i);
+            };
+        }
+    }
+}
