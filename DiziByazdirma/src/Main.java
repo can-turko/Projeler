ed3a3c5f61036580bb11f7a6fc552e64d57534b7
Can Turko <turkoglu.can@gmail.com>
2022-09-13 17:11:57 +0300

    Dizi ile ekrana B yazdırma eklendi

---
+++ DiziByazdirma/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        String[][] arr = new String[7][4];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (i == 0 || i == 3 || i == 6) arr[i][j] = " * ";
+                else if (j == 0 || j == 3)arr[i][j] = " * ";
+                else arr[i][j]= "   ";
+            }
+        }
+        for(String[] a : arr){
+            for(String b : a){
+                System.out.print(b);
+            }
+            System.out.println();
+        }
+
+    }
+}
