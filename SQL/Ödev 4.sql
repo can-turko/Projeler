77f553ad4b493e0a6130b7a9ef385d52e83ea4fd
Can Turko <turkoglu.can@gmail.com>
2022-09-27 09:31:17 +0300

    SQL ödev 4 eklendi

---
+++ SQL/Ödev 4.sql
@@ -0,0 +1,12 @@
+--SELECT DISTINCT replacement_cost FROM film;
+
+--SELECT COUNT(DISTINCT replacement_cost) FROM film;
+
+--SELECT * FROM film
+--WHERE title ILIKE 't%' AND rating = 'G';
+
+--SELECT COUNT(*) FROM country
+--WHERE country ILIKE '_____';
+
+--SELECT * FROM city
+--WHERE city ILIKE '%R';
